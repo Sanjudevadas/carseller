2b756037f02d0589fcb988440975269f3e702f3e
Sanju Devadas <sanjudevadas11@gmail.com>
2025-04-09 21:15:34 +0530

    adding some sqlite codes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+//use App\Models\Car;
+use App\Models\Car;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+
+  
     public function index(){
-        return view ('index');
+  $cars= Car::get();
+  dump($cars);
+         return view ('index');
     }
 }
