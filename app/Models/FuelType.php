bded701f2068e8fdf3c2bde47f9e29e02b6e2755
Sanju Devadas <sanjudevadas11@gmail.com>
2025-04-09 11:49:20 +0530

    adding ORM models

---
+++ app/Models/FuelType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FuelType extends Model
+{
+    use HasFactory;
+
+//    protected $table= 'car_fuel_types';
+
+
+//protected $primaryKey = 'fuel_type_id';
+
+   public $timestamps = false;  
+}
